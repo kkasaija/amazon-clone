9c04ab0cf5a261bfc91f141ba61061eab0fe8f3e
Kasaija Kenneth <kasaijak@gmail.com>
2024-06-06 09:43:51 +0300

    add spec tests for currency formater

---
+++ test-jasmine/spec/money.spec.js
@@ -0,0 +1,19 @@
+import {formatCurrency} from '../../scripts/utils/money.js'
+
+describe('Test suite: formatCurrency()', ()=>{
+  it('should convert cents to dollars: ', ()=>{
+    expect(formatCurrency(2095)).toEqual('20.95')
+  });
+
+  it('should work with zeros(0):', ()=>{
+    expect(formatCurrency(0)).toEqual('0.00')
+  });
+
+  it('should round to the nearest cent:', ()=>{
+    expect(formatCurrency(2000.5)).toEqual('20.01')
+  });
+
+  it('should round to the nearest cent:', ()=>{
+    expect(formatCurrency(2000.4)).toEqual('20.00')
+  });
+})
